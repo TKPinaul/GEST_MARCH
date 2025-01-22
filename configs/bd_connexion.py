2b055ee6ec3418cb63be507f6bd3824c4cd419a6
TKPinaul <pkt.paulin@gmail.com>
2025-01-22 15:10:05 +0000

    correction of dbname variable to bdname

--- configs/bd_connexion.py
+++ configs/bd_connexion.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 def get_database():
    # Récupérer les informations de connexion à la base de données à partir de .env
    uri = config('BDURL')
-   db_name = config('DBNAME')
+   db_name = config('BDNAME')
 
    # Connexion à la base de données
    client = MongoClient(uri)
