94b4e3614cb650858e456cb4ba081792724874c9
TKPinaul <pkt.paulin@gmail.com>
2025-01-22 13:16:57 +0000

    configuration connexion

---
+++ configs/bd_connexion.py
@@ -0,0 +1,15 @@
+from decouple import config
+from pymongo import MongoClient
+
+
+def get_database():
+   # Récupérer les informations de connexion à la base de données à partir de .env
+   uri = config('BDURL')
+   db_name = config('DBNAME')
+
+   # Connexion à la base de données
+   client = MongoClient(uri)
+
+   # Sélectionnez la base de données
+   db = client[db_name]
+   return db
