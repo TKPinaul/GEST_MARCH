d26cc607bd2525d5016b420b4d5f4ea94623cad2
TKPinaul <pkt.paulin@gmail.com>
2025-01-26 16:04:41 +0000

    update models

--- GEST_MARCH/models/marche.py
+++ GEST_MARCH/models/marche.py
@@ -7,39 +7,13 @@ class Marche:
    
    def __init__(self, nom_marche, x_ligne=50, y_colonne=50, marche_id=None):
       """ Initialisation d'un marché matriciel de taille x_ligne * y_colonne par defaut 50*50 """
-      self.marche_id = marche_id if marche_id else str(uuid.uuid4) # Générer un id aléatoire si non fourni
+      self.marche_id = marche_id if marche_id else str(uuid.uuid4()) # Générer un id aléatoire si non fourni
       self.nom_marche = nom_marche
       self.x_ligne = x_ligne
       self.y_colonne = y_colonne
       
       self.grille = [[False for _ in range(self.x_ligne)] for _ in range(self.y_colonne)] # False = pas de marchand, True = marchand
-      
-   def stand_available(self, x, y):
-      """ Vérifie si un stand est disponible """
-      if (0 <= x < self.x_ligne) and (0 <= y < self.y_colonne):
-         return not self.grille[x][y]
-      else:
-         raise ValueError(f"Les coordonnées (x, y) sont hors de la grille")
-   
-   def occup_stand(self, x, y):
-      """ Occuper un stand """
-      if self.stand_available(x, y):
-         self.grille[x][y] = True
-      else:
-         raise ValueError(f"Le stand ({x}, {y}) est déjà occupé ou hors de la grille")
-   
-   def free_stand(self, x, y):
-      """ Libérer un stand """
-      if (0 <= x < self.x_ligne) and (0 <= y < self.y_colonne):
-         self.grille[x][y] = False
-      else:
-         raise ValueError(f"Les coordonnées (x, y) sont hors de la grille")
-   
-   def show_grille(self):
-      """ Afficher la grille du marché """
-      for stand in self.grille:
-         print(stand)
-         
+     
    def save(self):
       """Enregistre un marché dans la base de données"""
       db = get_database()
@@ -52,6 +26,37 @@ class Marche:
          'grille': self.grille
       }
       collection.insert_one(data) # Insérer le marché dans la base de données
+      
+    
+   def stand_available(self, x, y):
+      """ Vérifie si un stand est disponible """
+      if (0 <= x < self.x_ligne) and (0 <= y < self.y_colonne):
+         return not self.grille[x][y]
+      else:
+         raise ValueError(f"Les coordonnées (x, y) sont hors de la grille")
+   
+   
+   def occup_stand(self, x, y):
+      """ Occuper un stand """
+      if self.stand_available(x, y):
+         self.grille[x][y] = True
+      else:
+         raise ValueError(f"Le stand ({x}, {y}) est déjà occupé ou hors de la grille")
+   
+   
+   def free_stand(self, x, y):
+      """ Libérer un stand """
+      if (0 <= x < self.x_ligne) and (0 <= y < self.y_colonne):
+         self.grille[x][y] = False
+      else:
+         raise ValueError(f"Les coordonnées (x, y) sont hors de la grille")
+   
+   
+   def show_grille(self):
+      """ Afficher la grille du marché """
+      for stand in self.grille:
+         print(stand)
+         
    
    @staticmethod
    def get_one(marche_id):
