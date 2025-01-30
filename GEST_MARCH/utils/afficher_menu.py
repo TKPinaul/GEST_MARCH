28e2f3219a83bb50c269e731f380d2b4ca74539e
TKPinaul <pkt.paulin@gmail.com>
2025-01-30 09:26:25 +0000

    function View Questionnaire

---
+++ GEST_MARCH/utils/afficher_menu.py
@@ -0,0 +1,56 @@
+def marche_menu():
+   print(
+      """
+      === Gestions des Marchés ===
+      1. Créer un marché
+      2. Afficher tous les marchés
+      3. Afficher la grille d'un marché
+      4. Quitter
+      """
+   )
+   
+def marchand_menu():
+   print(
+      """
+      === Gestions des Marchands ===
+      1. Créer un marchand
+      2. Afficher tous les marchands
+      3. Afficher un marchand
+      4. Modifier un marchand
+      5. Supprimer un marchand
+      6. Afficher les marchés
+      7. Afficher la grille d'un marché
+      8. Affecté un marchand a un marché
+      9. Quitter
+      """
+   )
+
+def transactionEntre_menu():
+   print(
+   """
+   === Gestion des transaction d'entre ===
+   1. Ravitaillement de stock
+   2. Affichage des transactions d'entrée
+   3. Affichage des transaction d'entrée d'un marchand
+   4. Afficher les transaction d'entrée d'un produits
+   5. Afficher les transactions ayant fais l'objet d'un montant minimum
+   6. Afficher les transactions ayant fais l'objet d'un montant maximum
+   7. Afficher les transactions ayant fais l'objet d'un montant (minimum - maximum)
+   8. Quitter
+   """
+   )
+
+def transactionSortie_menu():
+   print(
+   """
+   === Gestion des transaction d'entre ===
+   1. Vendre produit
+   2. Affichage des transactions de sorties
+   3. Affichage des transactions de sorties d'un marchand
+   4. Afficher les transactions d'entrée d'un produits
+   5. Afficher les transactions ayant fais l'objet d'un montant minimum
+   6. Afficher les transactions ayant fais l'objet d'un montant maximum
+   7. Afficher les transactions ayant fais l'objet d'un montant (minimum - maximum)
+   8. Quitter
+   """
+   )
